7d538091773f54bea8bce7665e8d34930db1fd5a
UDHAYA4777 <123248288+UDHAYA4777@users.noreply.github.com>
2025-03-19 17:50:23 +0530

    Add files via upload

---
+++ AI ChatBot/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { Assistant } from "./assistants/openai";
+import { Loader } from "./components/Loader/Loader";
+import { Chat } from "./components/Chat/Chat";
+import { Controls } from "./components/Controls/Controls";
+import styles from "./App.module.css";
+
+function App() {
+  const assistant = new Assistant();
+  const [messages, setMessages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  function addMessage(message) {
+    setMessages((prevMessages) => [...prevMessages, message]);
+  }
+
+  async function handleContentSend(content) {
+    addMessage({ content, role: "user" });
+    setIsLoading(true);
+    try {
+      const result = await assistant.chat(content, messages);
+      addMessage({ content: result, role: "assistant" });
+    } catch {
+      addMessage({
+        content: "Sorry, I couldn't process your request. Please try again!",
+        role: "system",
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <div className={styles.App}>
+      {isLoading && <Loader />}
+      <header className={styles.Header}>
+        <img className={styles.Logo} src="/chat-bot.png" />
+        <h2 className={styles.Title}>Study Buddy</h2>
+      </header>
+      <div className={styles.ChatContainer}>
+        <Chat messages={messages} />
+      </div>
+      <Controls isDisabled={isLoading} onSend={handleContentSend} />
+    </div>
+  );
+}
+
+export default App;
---
+++ AI ChatBot/src/components/Chat/Chat.jsx
@@ -0,0 +1,58 @@
+import { useRef, useEffect, useMemo } from "react";
+import PropTypes from "prop-types"; 
+import Markdown from "react-markdown";
+import styles from "./Chat.module.css";
+
+const WELCOME_MESSAGE_GROUP = [
+  {
+    role: "assistant",
+    content: "Hello! How can I assist you right now?",
+  },
+];
+
+export function Chat({ messages }) {
+  const messagesEndRef = useRef(null);
+  const messagesGroups = useMemo(
+    () =>
+      messages.reduce((groups, message) => {
+        if (message.role === "user") groups.push([]);
+        groups[groups.length - 1].push(message);
+        return groups;
+      }, []),
+    [messages]
+  );
+
+  useEffect(() => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+
+  return (
+    <div className={styles.Chat}>
+      {[WELCOME_MESSAGE_GROUP, ...messagesGroups].map(
+        (messages, groupIndex) => (
+       
+          <div key={groupIndex} className={styles.Group}>
+            {messages.map(({ role, content }, index) => (
+              
+              <div key={index} className={styles.Message} data-role={role}>
+                <Markdown>{content}</Markdown>
+              </div>
+            ))}
+          </div>
+        )
+      )}
+
+      <div ref={messagesEndRef} />
+    </div>
+  );
+}
+
+
+Chat.propTypes = {
+  messages: PropTypes.arrayOf(
+    PropTypes.shape({
+      role: PropTypes.string.isRequired,
+      content: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
